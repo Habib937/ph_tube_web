7ec6124f29ffb7590c8503d64fb4620ce7757b3b
1987_ Mohammad Habib Ullah <mohammadhabibullah937@gmail.com>
2024-11-27 00:58:55 +0600

    search work complete

--- script/video.js
+++ script/video.js
@@ -29,10 +29,10 @@ const loadCatagories = () => {
     .catch((error) => console.log(error));
 };
 
-const loadVideos = () => {
+const loadVideos = (searchText ="") => {
     // fetch the data
 // create load
-    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
     .then((res) => res.json())
     .then((data) => displayVideos(data.videos))
     .catch((error) => console.log(error));
@@ -159,5 +159,9 @@ const displayCatagories = (categories) => {
  });
 
 };
+
+document.getElementById("search-input").addEventListener("keyup", (e) =>{
+    loadVideos(e.target.value)
+})
 loadCatagories();
 loadVideos();
